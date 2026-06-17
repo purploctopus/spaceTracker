26a6d7a824b969edecca6dd7fc151d2022ec3617
PurplOctopus <purploctopus@gmail.com>
2026-06-17 15:05:39 -0500

    Initial Commit

---
+++ spaceTracker/spaceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceTrackerApp.swift
+//  spaceTracker
+//
+//  Created by Ben Clary on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct spaceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
